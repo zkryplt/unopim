69bed7f26daefbce41bc71d8c7206d62ffb63a18
zkryplt <eposta@zekeriyapolat.com.tr>
2025-12-29 16:48:15 +0300

    Swap boot and register methods in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,26 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\ParallelTesting;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
-
-        ParallelTesting::setUpTestDatabase(function (string $database, int $token) {
-            Artisan::call('db:seed');
-        });
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
-
-    /**
-     * Register any application services.
-     */
-    public function register(): void {}
 }
